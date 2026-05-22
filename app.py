967d6ac5c53997a725aff6ec1b696c7de0a1477e
lassenza29 <lassenza29@gmail.com>
2026-05-22 15:31:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ import time
 # CONFIGURATION
 # ==============================================================================
 st.set_page_config(
-    page_title="Alpha Terminal Pro",
+    page_title="",
     page_icon="📈",
     layout="wide",
     initial_sidebar_state="expanded"
